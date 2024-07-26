e02ebc16f718c8fbafa4cfb3247494294703a938
ail3n <ailenrgrimaldi@gmail.com>
2024-07-26 00:38:23 -0300

    Add sidebar to authenticated layout and delete it from onboard

--- src/layouts/authenticated.rs
+++ src/layouts/authenticated.rs
@@ -1,14 +1,18 @@
 use dioxus::prelude::*;
 
-use crate::{components::molecules::Header, pages::route::Route};
+use crate::{components::molecules::{Header, Sidebar}, pages::route::Route};
 
 #[component]
 pub fn Authenticated() -> Element {
     rsx! {
         div { class: "page",
-            div { class: "layout layout--authenticated grid-header",
-                Header {}
+            div {
+                class: "layout layout--onboard grid-sidebar",
+                Sidebar {}
             }
+            div { class: "layout layout--authenticated grid-header",
+                Header {},
+            },
             Outlet::<Route> {}
         }
     }
